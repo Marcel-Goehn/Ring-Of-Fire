771ce8fa4e45ba63adfb119886c373c677a6b79e
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-14 15:17:11 +0200

    Added start game button to the start screen and created a game component. Also created a newGame method that changes the route to the game screen

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class StartscreenComponent {
 
+  constructor(private router: Router) {}
+
+  
+  newGame() {
+    this.router.navigateByUrl('/game');
+  }
 }
