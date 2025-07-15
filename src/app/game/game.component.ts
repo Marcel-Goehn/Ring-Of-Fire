b8986fc973d6a6d4aef8fd266567ceaf93b99af9
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-15 15:24:41 +0200

    Added a game model

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
-
+export class GameComponent implements OnInit{
+  game!: Game;
   pickCardAnimation: boolean = false;
 
-  takeCard() {
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+
+  newGame(): void {
+    this.game = new Game;
+    console.log(this.game);
+  }
+
+
+  takeCard(): void {
     this.pickCardAnimation = true;
   }
 }
